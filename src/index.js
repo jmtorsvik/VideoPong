d0661c8a19016325b3fba4fc4d4570c548d7516f
heleneyj <heleneyj@stud.ntnu.no>
2022-04-01 13:54:21 +0200

    Add video layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Video from "./component/Video";
+import Register from "./component/Register"
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+        <Routes>
+            <Route path="/" element={<Register />} />
+            <Route path="/game" element={<App />} />
+            <Route path="/video" element={<Video />} />
+        </Routes>
+    </Router>,
   document.getElementById("root")
 );
 
