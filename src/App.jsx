53a8aaedcf9385829ae3f5453afac454f31e3abd
heleneyj <heleneyj@stud.ntnu.no>
2022-04-08 23:40:53 +0200

    Add layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,107 +2,108 @@ import { useEffect, useRef } from "react";
 import "./App.css";
 
 export default function App() {
-    const ref = useRef();
-    useEffect(() => {
-        if (ref.current) {
-            const canvas = ref.current;
-            const context = canvas.getContext("2d");
-            const grid = 15;
-            const playerHeight = grid * 5; // 45
-            const maxPlayerY = canvas.height - grid - playerHeight;
+  const ref = useRef();
+  useEffect(() => {
+    if (ref.current) {
+      const canvas = ref.current;
+      const context = canvas.getContext("2d");
+      const grid = 15;
+      const playerHeight = grid * 5; // 45
+      const maxPlayerY = canvas.height - grid - playerHeight;
 
-            const playerSpeed = 5;
+      const playerSpeed = 5;
 
-            const leftPlayer = {
-                // start in the middle of the game on the left side
-                x: 0,
-                y: canvas.height / 2 - playerHeight / 2,
-                width: grid,
-                height: playerHeight,
-                // player velocity
-                dy: 0,
-            };
+      const leftPlayer = {
+        // start in the middle of the game on the left side
+        x: 0,
+        y: canvas.height / 2 - playerHeight / 2,
+        width: grid,
+        height: playerHeight,
+        // player velocity
+        dy: 0,
+      };
 
-            const rightPlayer = {
-                // start in the middle of the game on the right side
-                x: canvas.width - grid,
-                y: canvas.height / 2 - playerHeight / 2,
-                width: grid,
-                height: playerHeight,
+      const rightPlayer = {
+        // start in the middle of the game on the right side
+        x: canvas.width - grid,
+        y: canvas.height / 2 - playerHeight / 2,
+        width: grid,
+        height: playerHeight,
 
-                // player velocity
-                dy: 0,
-            };
+        // player velocity
+        dy: 0,
+      };
 
-            // game loop
-            function loop() {
-                requestAnimationFrame(loop);
-                context.clearRect(0, 0, canvas.width, canvas.height);
+      // game loop
+      function loop() {
+        requestAnimationFrame(loop);
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-                // move players by their velocity
-                leftPlayer.y += leftPlayer.dy;
-                rightPlayer.y += rightPlayer.dy;
+        // move players by their velocity
+        leftPlayer.y += leftPlayer.dy;
+        rightPlayer.y += rightPlayer.dy;
 
-                // prevent players from going through walls
-                if (leftPlayer.y < grid) {
-                    leftPlayer.y = grid;
-                } else if (leftPlayer.y > maxPlayerY) {
-                    leftPlayer.y = maxPlayerY;
-                }
-
-                if (rightPlayer.y < grid) {
-                    rightPlayer.y = grid;
-                } else if (rightPlayer.y > maxPlayerY) {
-                    rightPlayer.y = maxPlayerY;
-                }
-
-                // draw player bars
-                context.fillStyle = "black";
-                context.fillRect(
-                    leftPlayer.x,
-                    leftPlayer.y,
-                    leftPlayer.width,
-                    leftPlayer.height
-                );
-                context.fillRect(
-                    rightPlayer.x,
-                    rightPlayer.y,
-                    rightPlayer.width,
-                    rightPlayer.height
-                );
-
-                // draw walls
-                context.fillStyle = "black";
-                context.fillRect(0, 0, canvas.width, grid);
-                context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
-            }
-
-            // listen to keyboard events to move the players
-            document.addEventListener("keydown", function (e) {
-                if (e.key === "ArrowUp") {
-                    rightPlayer.dy = -playerSpeed;
-                }
-                else if (e.key === "ArrowDown") {
-                    rightPlayer.dy = playerSpeed;
-                }
-            });
-
-            // listen to keyboard events to stop the player if key is released
-            document.addEventListener("keyup", function (e) {
-                if ((e.key === "ArrowUp" && rightPlayer.dy < 0) || (e.key === "ArrowDown" && rightPlayer.dy > 0)) {
-                    rightPlayer.dy = 0;
-                }
-
-            });
-
-            // start the game
-            requestAnimationFrame(loop);
+        // prevent players from going through walls
+        if (leftPlayer.y < grid) {
+          leftPlayer.y = grid;
+        } else if (leftPlayer.y > maxPlayerY) {
+          leftPlayer.y = maxPlayerY;
         }
-    }, []);
 
-    return (
-        <div>
-            <canvas width="750" height="585" id="game" ref={ref}></canvas>
-        </div>
-    );
+        if (rightPlayer.y < grid) {
+          rightPlayer.y = grid;
+        } else if (rightPlayer.y > maxPlayerY) {
+          rightPlayer.y = maxPlayerY;
+        }
+
+        // draw player bars
+        context.fillStyle = "black";
+        context.fillRect(
+          leftPlayer.x,
+          leftPlayer.y,
+          leftPlayer.width,
+          leftPlayer.height
+        );
+        context.fillRect(
+          rightPlayer.x,
+          rightPlayer.y,
+          rightPlayer.width,
+          rightPlayer.height
+        );
+
+        // draw walls
+        context.fillStyle = "black";
+        context.fillRect(0, 0, canvas.width, grid);
+        context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
+      }
+
+      // listen to keyboard events to move the players
+      document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowUp") {
+          rightPlayer.dy = -playerSpeed;
+        } else if (e.key === "ArrowDown") {
+          rightPlayer.dy = playerSpeed;
+        }
+      });
+
+      // listen to keyboard events to stop the player if key is released
+      document.addEventListener("keyup", function (e) {
+        if (
+          (e.key === "ArrowUp" && rightPlayer.dy < 0) ||
+          (e.key === "ArrowDown" && rightPlayer.dy > 0)
+        ) {
+          rightPlayer.dy = 0;
+        }
+      });
+
+      // start the game
+      requestAnimationFrame(loop);
+    }
+  }, []);
+
+  return (
+    <div>
+      <canvas width="750" height="585" id="game" ref={ref}></canvas>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Video from "./component/Video";
-import Register from "./component/Register"
+import Register from "./component/Register";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
-    <Router>
-        <Routes>
-            <Route path="/" element={<Register />} />
-            <Route path="/game" element={<App />} />
-            <Route path="/video" element={<Video />} />
-        </Routes>
-    </Router>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<Register />} />
+      <Route path="/game" element={<App />} />
+      <Route path="/video" element={<Video />} />
+    </Routes>
+  </Router>,
   document.getElementById("root")
 );
 
