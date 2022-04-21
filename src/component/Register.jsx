c44fbaa4a998a55ac2bd18c1a16cb27307cefda1
heleneyj <heleneyj@stud.ntnu.no>
2022-04-21 13:25:32 +0200

    Add register with localStorage

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -3,51 +3,43 @@ import "./css/register.css"; // Css gjøres i css fila og du can bruke class nam
 import {Button, Form} from "react-bootstrap";
 
 
+
 function Register() {
-
-    {/*
-  Dette er en react hook. De er fantastiske og funker slik at du kan lagre variabler der med den første const "input"
-  og endre det med å bruke nr 2 "setInput.
-  Du kan se bruken i eksempelet lengere ned
-  hver gang det skjer en endring i "state" vil den reloade siden"
-  */
-    }
     const [input, setInput] = useState("");
+    const [isRegistered, setIsRegistered] = useState(localStorage.getItem('komSys')); //Kan state settes når man har sjekket om det er lagret en tidligere innlogging i browser
 
-    const [isRegistered, setIsRegistered] = useState(false); //Kan state settes når man har sjekket om det er lagret en tidligere innlogging i browser
-
-    /*
-    noe kode som sjekker om de er registert først
-    settes med setIsRegistered()
-     */
-
-    //Funksjon av ting som gjøres etter at noen har trykket på knappen
-    //Burde sjekke at input ikke er null eller en tom streng.
     function onSubmitPress() {
-
+        localStorage.setItem('komSys', input);
+        setIsRegistered(input)
     }
 
-    // has not registered before
+
     if (!isRegistered) {
         return (
             <div className="register-main">
-                <h1>Hello</h1>
-                <Form.Control
-                    type="text"
-                    id="name"
-                    value={input}
-                    onChange={e => setInput(e.target.value)}
-                />
+                <div className="register-container">
+                    <div className="register-content">
+                        <h1>Name</h1>
+                        <Form.Control
+                            type="text"
+                            id="name"
+                            className="input-name"
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
+                        />
 
-                {/* nå kaller den bare console.log, men den skal jo helst kalle den funksjonen den trenger*/}
-                <Button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={() => console.log(input)}
-                >
-                    Submit
-                </Button>
+                        <Button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={() => onSubmitPress()}
+                        >
+                            Submit
+                        </Button>
+                    </div>
+                </div>
+
             </div>
+
         );
     } else {
         return window.location.replace("/video");
