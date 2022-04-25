705da567bce9e44ca4fcf32949bef1c618288237
Herman Henriksen <hermanimittveien@gmail.com>
2022-04-25 11:37:30 +0200

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,11 @@ export default function App() {
   useEffect(() => {
     client.on("connect", function () {
       client.subscribe("/ponggame/#");
-      console.log("CONNECTED");
       client.on("message", (topic, message, packet) => {
-        console.log(topic);
         if (topic === "/ponggame/new_user") {
           const { username } = JSON.parse(message);
           if (username !== localStorage.getItem("username")) {
             setParticipants((prevState) => [...prevState, username]);
-            client.publish(
-              "/ponggame/new_user",
-              JSON.stringify({ username: localStorage.getItem("username") })
-            );
           }
         }
       });
