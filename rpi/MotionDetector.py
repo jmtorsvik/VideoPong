4b368427daef6bfb49145c3bfb379bc8fc3a8346
Jakob <jmtorsvik@gmail.com>
2022-04-26 21:28:08 +0200

    fixed motion detector

--- rpi/MotionDetector.py
+++ rpi/MotionDetector.py
@@ -4,67 +4,91 @@ from mqtt_client import broker, port
 from stmpy import Machine
 from threading import Thread
 
+
 class MotionDetector:
 
     def __init__(self):
         self.running = False
 
     def start_detecting(self):
+        # set running to true and start a detection thread that runs as long as running is true
         self.running = True
         Thread(target=self.detect).start()
 
     def stop_detecting(self):
+        # set running to false which will stop the running detection thread
         self.running = False
 
     def start_video(self):
+        # send an mqtt message that starts an instance of the video chat on the office ocmputer
         print("Video chat started")
         mqttc = Client(transport="websockets")
         mqttc.connect(broker, port)
         mqttc.publish("ponggame/office_start")
 
     def detect(self):
-        print("Started detecting")
         # Motion detection inspired by https://www.geeksforgeeks.org/webcam-motion-detector-python/
-        back_frame = None
-        video = cv2.VideoCapture(0)
+        print("Started detecting")
+        back_frame = None  # background frame for comparison
+        video = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # openCV video capture
         while (self.running):
-            _, frame = video.read()
-            detected = False
+            # retrieve current frame
+            _, frame = video.read() 
+            
+            detected = False # boolean indicating observed detection
 
+            # create a GaussianBlur image for easier comparison
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
+            # set initial background frame
             if back_frame is None:
                 back_frame = gray_frame
                 continue
             
+            # compare current frame with background frame
             diff_frame = cv2.absdiff(back_frame, gray_frame)
-            # Threshold value is currently set to 100. The optimal threshold value may vary from space to space
-            thresh_frame = cv2.threshold(diff_frame, 100, 255, cv2.THRESH_BINARY)[1]
-            thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
-            cnts,_ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # create a black-white frame showing the observed changed
+            # threshold value is currently set to 60. The most suiting threshold value may vary from space to space
+            thresh_frame = cv2.threshold(
+                diff_frame, 60, 255, cv2.THRESH_BINARY)[1]
+            thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+            # find the contours of object in motion
+            cnts, _ = cv2.findContours(
+                thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            
+            # search for observed change in contours
             for contour in cnts:
                 if cv2.contourArea(contour) < 10000:
                     continue
                 detected = True
 
+            # alert detected motion
             if (detected):
                 print("Motion detected!")
                 self.stm.send("motion_detected")
 
+            # end motion detection if 'q' is entered on keyboard
             key = cv2.waitKey(1)
             if key == ord('q'):
                 break
-            
-            # set new background to current frame
+
+            # set new background frame to current frame
             back_frame = gray_frame
 
+            # display black-white frame
             cv2.imshow("Difference Frame", diff_frame)
 
+        # stop motion detection
         video.release()
         cv2.destroyAllWindows()
-        print("Stopped detecting")             
+        print("Stopped detecting")
+
+    def test_print(self, txt):
+        print(txt)
+
 
 motion_detector = MotionDetector()
 
@@ -72,55 +96,58 @@ motion_detector = MotionDetector()
 t0 = {'source': 'initial',
       'target': 'detecting',
       'effect': 'start_detecting'
-}
+      }
 
-t1 = {'trigger':'motion_detected',
-      'source':'detecting',
-      'target':'wait',
-}
+t1 = {'trigger': 'motion_detected',
+      'source': 'detecting',
+      'target': 'wait'
+      }
 
-t2 = {'trigger':'t', 
-      'source':'wait', 
-      'target':'detected', 
-}
+t2 = {'trigger': 't',
+      'source': 'wait',
+      'target': 'detected'
+      }
 
-t3 = {'trigger':'motion_detected', 
-      'source':'detected', 
-      'target':'idle', 
+t3 = {'trigger': 'motion_detected',
+      'source': 'detected',
+      'target': 'idle',
       'effect': 'start_video; stop_timer("t")'
-}
+      }
 
-t4 = {'trigger':'t', 
-      'source':'detected', 
-      'target':'detecting',
-}
+t4 = {'trigger': 't',
+      'source': 'detected',
+      'target': 'detecting'
+      }
 
-t5 = {'trigger':'stop', 
-      'source':'idle', 
-      'target':'detecting', 
-}
+t5 = {'trigger': 'stop',
+      'source': 'idle',
+      'target': 'detecting'
+      }
 
-t6 = {'trigger':'start_video', 
-      'source':'detecting', 
-      'target':'idle', 
-}
+t6 = {'trigger': 'start_video',
+      'source': 'detecting',
+      'target': 'idle'
+      }
 
 # define states
-detecting = {'name': 'detecting'}
+detecting = {'name': 'detecting',
+             'entry': 'test_print("state: DETECTING")'
+             }
 
 wait = {'name': 'wait',
-    'entry': 'stop_detecting; start_timer("t", 5000)'
-}
+        'entry': 'stop_detecting; start_timer("t", 5000); test_print("state: WAIT")'
+        }
 
 detected = {'name': 'detected',
-    'entry': 'start_detecting; start_timer("t", 5000)'
-}
+            'entry': 'start_detecting; start_timer("t", 15000); test_print("state: DETECTED")'
+            }
 
 idle = {'name': 'idle',
-    'entry': 'stop_detecting',
-    'exit': 'start_detecting'
-}
+        'entry': 'stop_detecting; test_print("state: IDLE")',
+        'exit': 'start_detecting'
+        }
 
 # define state machine
-stm_motion_detector = Machine(name='stm_motion_detector', transitions=[t0, t1, t2, t3, t4, t5, t6], states=[detecting, wait, detected, idle], obj=motion_detector)
+stm_motion_detector = Machine(name='stm_motion_detector', transitions=[
+                              t0, t1, t2, t3, t4, t5, t6], states=[detecting, wait, detected, idle], obj=motion_detector)
 motion_detector.stm = stm_motion_detector
--- rpi/test_client.py
+++ rpi/test_client.py
@@ -1,9 +1,6 @@
 import paho.mqtt.client as mqtt
-import time
 
 client = mqtt.Client(transport="websockets")
 client.connect("broker.emqx.io", 8083)
 
-while True:
-    client.publish("/ponggame/exit_video")
-    time.sleep(5)
+client.publish("/ponggame/exit_video")
