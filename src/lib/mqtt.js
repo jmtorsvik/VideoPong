d8c3dde6aff7b6347feb1e2c7a6e8383c75482d6
Herman Henriksen <hermanimittveien@gmail.com>
2022-04-29 13:53:43 +0200

    Fixes

--- src/lib/mqtt.js
+++ src/lib/mqtt.js
@@ -1,5 +1,5 @@
 const mqtt = require("mqtt");
-const client = mqtt.connect("ws://broker.emqx.io:8083/mqtt", {
+const client = mqtt.connect("ws://broker.hivemq.com:8000", {
   keepalive: 60,
   clean: true,
 });
