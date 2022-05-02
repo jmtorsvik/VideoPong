c1f9cef014099cfe95d577ec7a9228b66a345d48
Herman Henriksen <hermanimittveien@gmail.com>
2022-05-02 10:25:43 +0200

    Fixes

--- rpi/mqtt_client.py
+++ rpi/mqtt_client.py
@@ -10,7 +10,7 @@ class MQTTClient:
         self.count = 0
         self.client = mqtt.Client(transport="websockets")
         self.client.username_pw_set("rJGaDmyOGwpZKzb0m4ILX5nqEmE6Kha1RGZkQlWlHb8hy96J1EkNzw12GvYeBW5i", "")
-
+        self.client.tls_set()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.stm_driver = stm_driver
